9ad766c7dbab965f01927f96d7481971a9f3d918
lukejeung <lukejeung11@dongyang.ac.kr>
2025-03-02 15:04:07 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./routes/Home";
+import Search from "./routes/Search";
+import Tv from "./routes/Tv";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path={["/", "/movies/:movieId"]}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
